2cd1bc136bce29c035286483a76ad44aa7828d98
Toby Joe Boudreaux <tj@tobyjoe.com>
2016-08-22 13:39:50 -0400

    Update iwconfig.sh

--- iwconfig.sh
+++ iwconfig.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget http://www.kernel.org/pub/software/network/iw/iw-3.17.tar.xz
+curl -O http://www.kernel.org/pub/software/network/iw/iw-3.17.tar.xz
 tar xpvf iw-3.17.tar.xz
 cd iw-3.17
 sudo make
