a1604a0f36879d96ffb67ed17f08ab13b39b40b8
Toby Joe Boudreaux <tj@tobyjoe.com>
2016-08-22 13:38:16 -0400

    Update piaircrackinstall.sh

--- piaircrackinstall.sh
+++ piaircrackinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-wget http://download.aircrack-ng.org/aircrack-ng-1.2-rc1.tar.gz
-tar -zxvf aircrack-ng-1.2-rc1.tar.gz
-cd aircrack-ng-1.2-rc1
+curl -O http://download.aircrack-ng.org/aircrack-ng-1.2-rc4.tar.gz
+tar -zxvf aircrack-ng-1.2-rc4.tar.gz
+cd aircrack-ng-1.2-rc4
 sudo apt-get -y install libnl1
 sudo apt-get -y install libnl-dev
 sudo apt-get -y install libnl-3-dev
